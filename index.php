e767480415c75d4800df7e282823f082b7ad6770
Francisco Graveli <fcdasj@hotmail.com>
2022-12-09 01:18:46 -0300

    Atualização index v2
    Atualização de imagens e responsividade do index, além de outros ajustes.

--- index.php
+++ index.php
@@ -11,11 +11,7 @@
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Google fonts-->
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
-    <link href="//db.onlinewebfonts.com/c/39459a22bc88390bf9f749d57984b3e1?family=CoreRhino75Heavy" rel="stylesheet" type="text/css" />
+    
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
 </head>
@@ -24,6 +20,7 @@
     @media (max-width: 360px) {
 
         .aboutUsButton {
+
             margin: 60px;
         }
     }
@@ -59,7 +56,7 @@
     </nav>
     <!-- Mashead header-->
     <header class="masthead bg-gradient-darkblue-to-lightblue ">
-        <div class="container px-5">
+        <div class="container px-5 py-7">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6">
                     <!-- Mashead text and app badges-->
@@ -73,37 +70,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <!-- Masthead device mockup feature-->
-                    <div class="masthead-device-mockup">
-                        <svg class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                            <defs>
-                                <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                                    <stop class="gradient-start-color" offset="0%"></stop>
-                                    <stop class="gradient-end-color" offset="100%"></stop>
-                                </linearGradient>
-                            </defs>
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg class="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
-                            <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect>
-                        </svg>
-                        <svg class="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <div class="device-wrapper">
-                            <div class="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
-                                <div class="screen bg-black">
-                                    <!-- PUT CONTENTS HERE:-->
-                                    <!-- * * This can be a video, image, or just about anything else.-->
-                                    <!-- * * Set the max width of your media to 100% and the height to-->
-                                    <!-- * * 100% like the demo example below.-->
-                                    <!--<video muted="muted" autoplay="" loop="" style="max-width: 100%; height: 100%"><source src="assets/img/demo-screen.mp4" type="video/mp4" /></video>-->
-                                    <img src="assets/img/updown.jpeg" style="max-width: 100%; height: 100%"></img>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <!-- Adiciona outros conteudos no header -->
                 </div>
             </div>
         </div>
@@ -126,45 +93,46 @@
         </div>
     </aside>
     <!-- App features section-->
-    <section id="features" style="scroll-behavior: smooth" class="bg-gradient-white-to-gray text-muted">
+    <section id="features" style="scroll-behavior: smooth" class=" bg-gradient-white-to-gray text-muted">
+    <div class="container bg-features" style="padding-top: 2rem; padding-bottom:2rem;">
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
-                <div class="col-lg-8 order-lg-1 mb-5 mb-lg-0">
-                    <div class="container-fluid px-5">
+                <div class="col-lg-8 order-lg-1 mb-5 mb-lg-0 featuresGridIcons">
+                    <div class="container-fluid px-5" >
                         <div class="row gx-5">
-                            <div class="col-md-6 mb-5">
+                            <div class="col-md-6 mb-5" style="max-width: 300px;">
                                 <!-- Feature item-->
                                 <div class="text-center">
-                                    <i class="bi-phone icon-feature text-gradient d-block mb-3"></i>
-                                    <h3 class="font-alt fs-5">Rastreio com GPS</h3>
-                                    <p class="text-muted mb-0 fs-6">Saiba exatamente onde se encontra sua encomenda!</p>
+                                    <img src="assets/img/features/celular.png" class=" d-block mb-1" style="max-width: 100%; height:auto;" />
+                                    <h3 class="font-alt fs-6 fw-bolder">Rastreio com GPS</h3>
+                                    <p class="text-muted mb-0 fs-6 fw-lighter">Saiba exatamente onde se encontra sua encomenda!</p>
                                 </div>
                             </div>
-                            <div class="col-md-6 mb-5">
+                            <div class="col-md-6 mb-5" style="max-width: 300px;">
                                 <!-- Feature item-->
                                 <div class="text-center">
-                                    <i class="bi-currency-dollar icon-feature text-gradient d-block mb-3"></i>
-                                    <h3 class="font-alt fs-5">Entregas Rentáveis</h3>
-                                    <p class="text-muted mb-0 fs-6">O melhor custo benefício para suas entregas!</p>
+                                <img src="assets/img/features/caminhao.png" class=" d-block mb-1" style="max-width: 100%; height:auto;" />
+                                    <h3 class="font-alt fs-6 fw-bolder">Entregas Rentáveis</h3>
+                                    <p class="text-muted mb-0 fs-6 fw-lighter">O melhor custo benefício para suas entregas!</p>
                                 </div>
                             </div>
                         </div>
                         <div class="row gx-5">
-                            <div class="col-md-6 mb-5 mb-md-0">
+                            <div class="col-md-6 mb-5 mb-md-0" style="max-width: 300px;">
                                 <!-- Feature item-->
                                 <div class="text-center">
-                                    <i class="bi-truck icon-feature text-gradient d-block mb-3"></i>
-                                    <h3 class="font-alt fs-5">Entregas flexiveís</h3>
-                                    <p class="text-muted mb-0 fs-6">Faça entregas sem 
+                                <img src="assets/img/features/cifrao.png" class=" d-block mb-1" style="max-width: 100%; height:auto;" />
+                                    <h3 class="font-alt fs-6 fw-bolder">Entregas flexiveís</h3>
+                                    <p class="text-muted mb-0 fs-6 fw-lighter">Faça entregas sem 
                                         limites de cargas, de envelopes à mudança residencial</p>
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-6" style="max-width: 300px;">
                                 <!-- Feature item-->
                                 <div class="text-center">
-                                    <i class="bi-emoji-sunglasses-fill icon-feature text-gradient d-block mb-3"></i>
-                                    <h3 class="font-alt fs-5 ">Suporte 24/7</h3>
-                                    <p class="text-muted mb-0 fs-6">Prestamos serviços para melhor atender a sua demanda
+                                <img src="assets/img/features/smile.png" class=" d-block mb-1" style="max-width: 100%; height:auto;" />
+                                    <h3 class="font-alt fs-6 fw-bolder ">Suporte 24/7</h3>
+                                    <p class="text-muted mb-0 fs-6 fw-lighter">Prestamos serviços para melhor atender a sua demanda
                                         de entregas. Na hora que precisar.</p>
                                 </div>
                             </div>
@@ -173,38 +141,11 @@
                 </div>
                 <div class="col-lg-4 order-lg-0">
                     <!-- Features section device mockup-->
-                    <div class="features-device-mockup">
-                        <svg class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                            <defs>
-                                <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                                    <stop class="gradient-start-color" offset="0%"></stop>
-                                    <stop class="gradient-end-color" offset="100%"></stop>
-                                </linearGradient>
-                            </defs>
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg class="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
-                            <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect>
-                        </svg>
-                        <svg class="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <div class="device-wrapper">
-                            <div class="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
-                                <div class="screen bg-black">
-                                    <!-- PUT CONTENTS HERE:-->
-                                    <!-- * * This can be a video, image, or just about anything else.-->
-                                    <!-- * * Set the max width of your media to 100% and the height to-->
-                                    <!-- * * 100% like the demo example below.-->
-                                    <img src="assets/img/app/descricaoProduto.jpg" style="max-width: 100%; height: 100%"></img>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div class="px-0 px-sm-0"><img class="img-fluid " id="imgfeatures" style="margin-left: auto; margin-right:auto;" src="assets/img/features/celularnamao.png" alt="..." /></div>
                 </div>
             </div>
         </div>
+    </div>
     </section>
     <!-- Basic features section-->
     <section class="bg-gradient-white-to-gray text-muted" id="AboutUs">
@@ -225,59 +166,53 @@
         </div>
     </section>
     <!-- Redes sociais começar aqui!!-->
-    <section class="bg-gradient-primary-to-secondary" id="socialMedia">
+    <section class="bg-gradient-blue-to-lightblue" id="socialMedia">
         <div class="container px-3">
             <h2 class="text-center text-white font-alt mb-4 fw-lighter">Além do nosso site,<br>
                 você poderá ficar por dentro de todas as nossas campanhas e novidades no:
             </h2>
-            <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center mx-5">
+            <div class="d-flex flex-wrap flex-lg-row align-items-center" id="socialID" style="justify-content: space-around;">
                 <div>
-                <a class=" text-center me-socialmedia mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/instagram.png" alt="Updown Usuários" /></a>
+                <a class=" text-center  mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/instagram.png" alt="Updown Usuários" /></a>
                 </div>
                 <div>
-                <a class=" text-center me-socialmedia mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/facebook.png" alt="Updown Usuários" /></a>
+                <a class=" text-center  mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/facebook.png" alt="Updown Usuários" /></a>
                 </div>
                 <div>
-                <a class=" text-center me-socialmedia mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/linkedin.png" alt="Updown Usuários" /></a>
+                <a class=" text-center  mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/linkedin.png" alt="Updown Usuários" /></a>
                 </div>
                 <div>
-                <a class=" text-center me-socialmedia mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/wpp.png" alt="Updown Usuários" /></a>
+                <a class=" text-center  mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/wpp.png" alt="Updown Usuários" /></a>
                 </div>
                 <div>
-                <a class=" text-center  me-socialmedia mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/mail.png" alt="Updown Usuários" /></a>
+                <a class=" text-center   mb-4 " href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="socialmedia" src="assets/img/social/mail.png" alt="Updown Usuários" /></a>
                 </div>
             </div>
         </div>
     </section>
     <!--  -->
     <!-- Call to action section-->
-    <section class="cta" id="Driver" style="background-image: url(https://source.unsplash.com/D-qq7W751vs/1500x800);">
+    <section class="cta" id="Driver">
         <div class="cta-content">
-            <div class="container px-5">
-                <h2 class="text-white display-3 lh-1 mb-4 upFont">
-                    Faça parte da
-                    <br />
-                    nossa rede.
+            <div class="container px-3">
+                <h2 class="text-white display-5 lh-1 mb-3 upFont  ">
+                    Simplicidade e Confiabilidade.
                 </h2>
-                <p class="text-light">Você decidirá seu horário e não terá tempo ocioso. <br></p>
-                <a class="btn btn-outline-light py-3 px-4 my-3 rounded-pill" href="driver.php">Conheça
-                    todos os benefícios</a>
+                <p class="text-light fw-lighter display-motorista ">Você decidirá seu horário e não terá tempo ocioso.<br></p>
+                <a class="btn btn-outline-driver py-4 px-4 my-3 ms-btn-driver rounded-pill fw-light" 
+                href="driver.php">Benefícios para motoristas</a>
             </div>
         </div>
     </section>
     
     <!-- App badge section-->
     <section class="bg-gradient-primary-to-secondary" id="download">
-        <div class="container px-5">
-            <h2 class="text-center text-white font-alt mb-4">Baixe agora o nosso aplicativo!</h2>
+        <div class="container px-5 text-center">
+            <h1 class="text-center text-white font-alt mb-3 fw-bold">Baixe agora o nosso aplicativo!</h1>
             <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
                 <div>
-                <p class="text-white">Seja um Usuário: </p>
-                <a class=" text-center me-lg-3 mb-4 mb-lg-0" href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="Updown Usuários" /></a>
-                </div>
-                <div>
-                <p class="text-white">Seja um Colaborador: </p>    
-                <a class=" text-center me-lg-3 mb-4 mb-lg-0" href="https://play.google.com/store/apps/details?id=com.updowndriver"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="Updown Colaboradores" /></a>
+                <h4><p class="text-white fw-lighter">Seja um Usuário: </p></h4>
+                <a class=" text-center me-lg-3 mb-4 mb-lg-0" href="https://play.google.com/store/apps/details?id=com.updownuser"><img class="app-badge" src="assets/img/pt-br_badge_web_generic.png" alt="Updown Usuários" /></a>
                 </div>
             </div>
         </div>
@@ -287,11 +222,11 @@
         <div class="container px-5">
             <div class="text-white-50 small">
                 <div class="mb-2">&copy; 2021 Updown - todos os direitos reservados.</div>
-                <a href="assets/docs/Politica.pdf">Política de Privacidade</a>
+                <a href="assets/docs/Politica.pdf" target="_blank">Política de Privacidade</a>
                 <span class="mx-1">&middot;</span>
-                <a href="assets/docs/termos de uso - clientes.pdf">Termos de Uso - Usuários</a>
+                <a href="assets/docs/termos de uso - clientes.pdf" target="_blank">Termos de Uso - Usuários</a>
                 <span class="mx-1">&middot;</span>
-                <a href="assets/docs/termos de uso - entregador.pdf.pdf">Termos de Uso - Entregadores</a>
+                <a href="assets/docs/termos de uso - entregador.pdf.pdf" target="_blank">Termos de Uso - Entregadores</a>
             </div>
         </div>
     </footer>
